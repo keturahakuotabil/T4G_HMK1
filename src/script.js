8d9d804fc5df305cbcabf5965e50cd6686f46372
keturahakuotabil <keturahakuotabil@gmail.com>
2026-07-20 09:40:15 -0400

    added changes to the script.js and index.html

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Prompt the user for their details
+let userName = prompt("Enter your name:");
+let userAge = prompt("Enter your age:");
+let userEmail = prompt("Enter your email:");
+
+// Display the information back to the user
+alert(
+    `Hi ${userName}, you are ${userAge} years old and we'll keep in touch via your email: ${userEmail}`
+);
