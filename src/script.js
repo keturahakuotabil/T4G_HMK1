67a15622a7e46746ab210dcfd8bffd963ff519e6
keturahakuotabil <keturahakuotabil@gmail.com>
2026-07-21 12:41:57 -0400

    added changes to script.js and index.html

--- src/script.js
+++ src/script.js
@@ -6,3 +6,44 @@ let userEmail = prompt("Enter your email:");
 // Display the information back to the user
 let message = `Hi ${userName}, you are ${userAge} years old and we'll keep in touch via your email: ${userEmail}`;
 alert(message);
+
+
+
+let name = "keturah";
+console.log("Name:", name);
+
+
+let age = 25;
+console.log("Age:", age);
+
+
+let addition = 15 + 10;
+console.log("Addition:", addition); // output addition:25
+
+
+let subtraction = 20 - 8;
+console.log("Subtraction:", subtraction); // output subtraction:12
+
+
+let division = 30 / 5;
+console.log("Division:", division); // output division:6
+
+
+let multiplication = 6 * 7;
+console.log("Multiplication:", multiplication); // output multiplication:42
+
+
+let remainder = 15 % 4;
+console.log("Remainder:", remainder); // output remainder:3
+
+
+let isStudent = true;
+console.log("Boolean:", isStudent); // output: Boolen: true 
+
+
+let numberValue = 100;
+console.log("Type of numberValue:", typeof numberValue); // output : Typeof numberValue: number
+
+
+let stringValue = "JavaScript";
+console.log("Type of stringValue:", typeof stringValue); // output: typeof stringvalue: string
